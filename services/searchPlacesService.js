0ff482220440464888f25f5ab44f240edb5e56ff
HiralGajreTechtic <techtic.hiralgajre@gmail.com>
2024-05-20 12:50:14 +0530

    get data from google place api if not present for lat long

--- services/searchPlacesService.js
+++ services/searchPlacesService.js
@@ -57,7 +57,46 @@ class SearchPlacesService {
         "geometry.location.lng": parseFloat(req.query.lng),
         "geometry.location.lat": parseFloat(req.query.lat),
       });
-      return addressData;
+      if (addressData?.length) {
+        return addressData;
+      } else {
+        let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${req.query.lat},${req.query.lng}&sensor=true&key=${apiKey}`;
+        let response = await axios.get(url);
+        let searchResponse = response.data;
+        for (let i in searchResponse.results) {
+          let placeDetails = await axios.get(
+            `https://maps.googleapis.com/maps/api/place/details/json?place_id=${searchResponse.results[i].place_id}&key=${apiKey}`
+          );
+          addressData.push({
+            _id: "",
+            address_components: placeDetails.data.result.address_components,
+            name: placeDetails.data.result.name,
+            formatted_address: placeDetails.data.result.formatted_address,
+            icon: placeDetails.data.result.icon
+              ? placeDetails.data.result.icon
+              : "",
+            photos: placeDetails.data.result.photos
+              ? placeDetails.data.result.photos
+              : [],
+            place_id: searchResponse.results[i].place_id,
+            plus_code: searchResponse.plus_code,
+            geometry: placeDetails.data.result.geometry,
+            rating: placeDetails.data.result.rating
+              ? placeDetails.data.result.rating
+              : 0,
+            types: placeDetails.data.result.types,
+            user_ratings_total: placeDetails.data.result.user_ratings_total
+              ? placeDetails.data.result.user_ratings_total
+              : 0,
+            image: "",
+            category_ids: [],
+            tag_ids: [],
+            updatedAt: null,
+            insertedAt: "",
+          });
+        }
+        return addressData;
+      }
     } catch (error) {
       throw new Error(error);
     }
